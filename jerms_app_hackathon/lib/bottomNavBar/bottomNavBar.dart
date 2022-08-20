703deb76912dedb73ac160316aa8c7b26d41cd25
Jeffry Chavenia <chaveniajeffry19@gmail.com>
2022-08-21 04:41:54 +0800

    nayaaaa

--- jerms_app_hackathon/lib/bottomNavBar/bottomNavBar.dart
+++ jerms_app_hackathon/lib/bottomNavBar/bottomNavBar.dart
@@ -1,1 +1,28 @@
- 
+import 'package:flutter/material.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:jerms_app_hackathon/getStarted/getStarted.dart';
+
+class BottomNavBar extends StatelessWidget {
+  const BottomNavBar({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+          // This is the theme of your application.
+          //
+          // Try running your application with "flutter run". You'll see the
+          // application has a blue toolbar. Then, without quitting the app, try
+          // changing the primarySwatch below to Colors.green and then invoke
+          // "hot reload" (press "r" in the console where you ran "flutter run",
+          // or simply save your changes to "hot reload" in a Flutter IDE).
+          // Notice that the counter didn't reset back to zero; the application
+          // is not restarted.
+          ),
+      home: const getStarted(),
+    );
+  }
+}
--- jerms_app_hackathon/lib/dashboard/startNavigating/startNavigating.dart
+++ jerms_app_hackathon/lib/dashboard/startNavigating/startNavigating.dart
@@ -1,14 +1,65 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_polyline_points/flutter_polyline_points.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:jerms_app_hackathon/dashboard/dashboard.dart';
 import 'package:jerms_app_hackathon/dashboard/startNavigating/navigateMallBluePrint/mapChangeView/mallMapView/mallMapView.dart';
+import 'package:location/location.dart';
 
+const LatLng SOURCE_LOCATION = LatLng(42.7477863,-71.1699932);
+const LatLng DEST_LOCATION = LatLng(42.744421,-71.1698939);
+const double CAMERA_ZOOM = 20;
+const double CAMERA_TILT = 80;
+const double CAMERA_BEARING = 30;
+const double PIN_VISIBLE_POSITION = 20;
+const double PIN_INVISIBLE_POSITION = -220;
 void main() {
   runApp(const StartNavigating());
 }
 
 
-class StartNavigating extends StatelessWidget {
+class StartNavigating extends StatefulWidget {
   const StartNavigating({super.key});
+
+  @override
+  State<StartNavigating> createState() => _StartNavigatingState();
+}
+
+class _StartNavigatingState extends State<StartNavigating> {
+  late GoogleMapController mapController;
+  final LatLng _center = const LatLng(45.521563, -122.677433);
+  late BitmapDescriptor sourceIcon;
+  late BitmapDescriptor destinationIcon;
+  Set<Marker> _markers =  Set<Marker>();
+  double pinPillPosition = PIN_VISIBLE_POSITION;
+  late LatLng currentLocation = LatLng(14.7649713, 120.9447974);
+  late LatLng destinationLocation;
+  bool userBadgeSelected = false;
+  final Location location = Location();
+  double rating = 30.0;
+  
+  StreamSubscription<LocationData>? _locationSub;               // new
+  Map<String, double>? _currentLocation;
+
+  Set<Polyline> _polylines = Set<Polyline>();
+  Set<Circle> circles = Set<Circle>();
+  List<LatLng> polylineCoordinates = [];
+  late PolylinePoints polylinePoints;
+  
+  
+  @override
+  void initState() {
+    _initLocationService();
+     _locationSub = location.onLocationChanged.listen((LocationData currentLoc) {
+      
+      debugPrint("${currentLoc.latitude} ${currentLoc.longitude}");
+      currentLocation = LatLng(currentLoc.latitude!.toDouble(), currentLoc.longitude!.toDouble());
+});
+    // TODO: implement initState
+    super.initState();
+    
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -16,32 +67,49 @@ class StartNavigating extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Start Navigating'),
       ),
-      body: Center(
-        child: Column(
-          children: <Widget>[
-            // Mall Blueprint View
-            ElevatedButton(
-              onPressed: () {
-                Navigator.push(context,
-                  MaterialPageRoute(
-                    builder: (context) => const MallMapView()
+      body: Stack(
+        children: <Widget>[
+        Positioned.fill(
+                  child: GoogleMap(
+                    zoomControlsEnabled: false,
+                    myLocationEnabled: true,
+                    compassEnabled: false,
+                    tiltGesturesEnabled: false,
+                    polylines: _polylines,
+                    markers: _markers,
+                    mapType: MapType.normal,
+                    circles: circles,
+                    initialCameraPosition: CameraPosition(
+                      zoom: CAMERA_ZOOM,
+                      bearing: CAMERA_BEARING,
+                      target: currentLocation
+                    ),
                   )
-                );
-              }, child:  const Text('Mall Map View'),
-            ),
-            // Map Change View
-            ElevatedButton(
-              onPressed: () {
-                Navigator.push(context,
-                  MaterialPageRoute(
-                    builder: (context) => Dashboard()
-                  )
-                );
-              }, child:  const Text('Go back to Dashboard'),
-            ),
-          ],
-        ),
-      ),
+                ),
+        ],
+      )
     );
+  }
+   Future _initLocationService() async {
+    var location = Location();
+
+    if (!await location.serviceEnabled()) {
+      if (!await location.requestService()) {
+        return;
+      }
+    }
+
+    var permission = await location.hasPermission();
+    if (permission == PermissionStatus.denied) {
+      permission = await location.requestPermission();
+      if (permission != PermissionStatus.granted) {
+        return;
+      }
+    }
+
+    var loc = await location.getLocation();
+    
+      debugPrint("${loc.latitude} ${loc.longitude}");
+
   }
 }
