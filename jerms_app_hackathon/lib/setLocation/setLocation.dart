0c2e645ef3ecccdba3c9ef6f0d5a7a97d3c7e4c7
elyssafaustino <81256713+elyssafaustino@users.noreply.github.com>
2022-08-20 18:48:53 +0800

    feat: JERMS-0003 - setLocation

--- jerms_app_hackathon/lib/setLocation/setLocation.dart
+++ jerms_app_hackathon/lib/setLocation/setLocation.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:jerms_app_hackathon/dashboard/dashboard.dart';
 import 'package:jerms_app_hackathon/main.dart';
 
 void main() {
@@ -10,19 +11,71 @@ class SetLocation extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    Color _color = const Color(0xFFBFFFFFF);
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Set Location'),
-      ),
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            Navigator.push(context,
-                MaterialPageRoute(builder: (context) => const MyApp()));
-            // Navigate back to first route when tapped.
-          },
-          child: const Text('Go back!'),
+        iconTheme: IconThemeData(
+          color: Color(0xFFBAA1945), //change your color here
         ),
+        backgroundColor: _color,
+        title: const Text(
+          'Set Location',
+          style: TextStyle(
+              color: Color(0xFFB322E2E),
+              fontFamily: 'Poppins Semi Bold',
+              fontSize: 16),
+        ),
+        centerTitle: true,
+      ),
+      body: Container(
+        child: Column(
+          children: <Widget>[
+            Container(
+                child: Container(
+                    padding: EdgeInsets.fromLTRB(30, 30, 30, 60),
+                    width: double.infinity,
+                    decoration: BoxDecoration(color: Color(0xFFBFFF7EB)),
+                    child: Container(
+                      child: TextField(
+                        textAlign: TextAlign.left,
+                        decoration: new InputDecoration(
+                          fillColor: Colors.white,
+                          filled: true,
+                          contentPadding: const EdgeInsets.all(10),
+                          suffixIcon: new Icon(
+                            Icons.search,
+                            color: Color(0xFFBC4C4C4),
+                          ),
+                          hintText: "Search here..",
+                          hintStyle: TextStyle(
+                              fontFamily: 'Poppins-Regular',
+                              fontSize: 14,
+                              color: Color(0xFFB707070)),
+                          enabledBorder: const OutlineInputBorder(
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(10.0)),
+                            borderSide: const BorderSide(
+                              color: Color(0xFFBC4C4C4),
+                            ),
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(10.0)),
+                            borderSide: BorderSide(color: Color(0xFFBC4C4C4)),
+                          ),
+                        ),
+                      ),
+                    ))),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.push(context,
+              MaterialPageRoute(builder: (context) => const Dashboard()));
+        },
+        backgroundColor: Colors.green,
+        child: Text("NEXT"),
       ),
     );
   }
