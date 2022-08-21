ae87e999b22c7925c2ccc274b0a38ca9e75e591c
Sairen Christian R. Buerano <sairenchristianrbuerano@gmail.com>
2022-08-21 20:09:04 +0800

    jerms-notes-messages-ui

--- jerms_app_hackathon/lib/notes/notes.dart
+++ jerms_app_hackathon/lib/notes/notes.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:jerms_app_hackathon/dashboard/dashboard.dart';
+import 'package:jerms_app_hackathon/notes/notesListItem/notesListItem.dart';
 
 void main() {
   runApp(const Notes());
@@ -10,20 +11,126 @@ class Notes extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    Color _color = const Color(0xFFBFFF7EB);
+
     return Scaffold(
         appBar: AppBar(
-          title: const Text('Notes'),
-        ),
-        body: Center(
-          child: ElevatedButton(
-            onPressed: () {
-              Navigator.push(context,
-                  MaterialPageRoute(builder: (context) => Dashboard()));
-              // Navigate back to first route when tapped.
-            },
-            child: const Text('Go to Dashboard'),
+          backgroundColor: _color,
+          title: const Text(
+            'Notes',
+            style: TextStyle(
+                fontSize: 16.0,
+                fontFamily: 'Poppins Medium',
+                color: Colors.black,
+                letterSpacing: 1),
           ),
+          centerTitle: true,
         ),
-    );
+        body: Container(
+            color: Color(0xFFBFFF7EB),
+            child: Column(children: <Widget>[
+              TextButton(onPressed: () {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => const NotesListItem()));
+                      }, child: Text("+ ADD NEW NOTES")),
+              Card(
+                  elevation: 0,
+                  shape: RoundedRectangleBorder(
+                    side: BorderSide(
+                      width: 330,
+                      color: Theme.of(context).colorScheme.outline,
+                    ),
+                    borderRadius: const BorderRadius.all(Radius.circular(3)),
+                  ),
+                  child: Row(children: <Widget>[
+                    Container(
+                      child: Image.asset("assets/images/notesColorRed.png",
+                          width: 10, height: 100, fit: BoxFit.cover),
+                    ),
+                    Container(
+                      child: Column(
+                        children: <Widget>[
+                          Container(
+                            margin: const EdgeInsets.fromLTRB(0, 0, 0, 0),
+                            child: Text(
+                                    ' Shopping List1\n' +
+                                    ' Sausage, Fillet, Ground Pork, Hotdog...',
+                                textAlign: TextAlign.left,
+                                style: TextStyle(
+                                    fontFamily: 'Poppins Bold',
+                                    fontSize: 12,
+                                    color: Color.fromARGB(250, 128, 128, 128))),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ])),
+              Card(
+                  elevation: 0,
+                  shape: RoundedRectangleBorder(
+                    side: BorderSide(
+                      width: 330,
+                      color: Theme.of(context).colorScheme.outline,
+                    ),
+                    borderRadius: const BorderRadius.all(Radius.circular(3)),
+                  ),
+                  child: Row(children: <Widget>[
+                    Container(
+                      child: Image.asset("assets/images/notesColorBlue.jpg",
+                          width: 10, height: 100, fit: BoxFit.cover),
+                    ),
+                    Container(
+                      child: Column(
+                        children: <Widget>[
+                          Container(
+                            margin: const EdgeInsets.fromLTRB(0, 0, 0, 0),
+                            child: Text(
+                                    ' Shopping List2\n' +
+                                    ' Chicken, Pork, Beef, Lamb...',
+                                textAlign: TextAlign.left,
+                                style: TextStyle(
+                                    fontFamily: 'Poppins Bold',
+                                    fontSize: 12,
+                                    color: Color.fromARGB(250, 128, 128, 128))),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ])),
+              Card(
+                  elevation: 0,
+                  shape: RoundedRectangleBorder(
+                    side: BorderSide(
+                      width: 330,
+                      color: Theme.of(context).colorScheme.outline,
+                    ),
+                    borderRadius: const BorderRadius.all(Radius.circular(3)),
+                  ),
+                  child: Row(children: <Widget>[
+                    Container(
+                      child: Image.asset("assets/images/notesColorViolet.png",
+                          width: 10, height: 100, fit: BoxFit.cover),
+                    ),
+                    Container(
+                      child: Column(
+                        children: <Widget>[
+                          Container(
+                            margin: const EdgeInsets.fromLTRB(0, 0, 0, 0),
+                            child: Text(
+                                    ' Shopping List3\n' +
+                                    ' Beer, Champagne, Vodka...',
+                                textAlign: TextAlign.left,
+                                style: TextStyle(
+                                    fontFamily: 'Poppins Bold',
+                                    fontSize: 12,
+                                    color: Color.fromARGB(250, 128, 128, 128))),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ])),
+            ])));
   }
 }
--- jerms_app_hackathon/lib/notes/notesListItem/notesListItem.dart
+++ jerms_app_hackathon/lib/notes/notesListItem/notesListItem.dart
@@ -1,28 +1,141 @@
 import 'package:flutter/material.dart';
+import 'package:jerms_app_hackathon/bottomNavBar/bottomNavBar.dart';
+import 'package:jerms_app_hackathon/dashboard/dashboard.dart';
+import 'package:jerms_app_hackathon/notes/notes.dart';
+import 'package:jerms_app_hackathon/notes/notesListItem/notesListItem.dart';
 
 void main() {
   runApp(const NotesListItem());
 }
-
 
 class NotesListItem extends StatelessWidget {
   const NotesListItem({super.key});
 
   @override
   Widget build(BuildContext context) {
+    Color _color = const Color(0xFFBFFF7EB);
+
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('NotesListItem'),
-      ),
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            Navigator.push(context,MaterialPageRoute(builder: (context) => const NotesListItem()));
-            // Navigate back to first route when tapped.
-          },
-          child: const Text('Go to Notes'),
+        appBar: AppBar(
+          backgroundColor: _color,
+          title: const Text(
+            'NotesListItem',
+            style: TextStyle(
+                fontSize: 16.0,
+                fontFamily: 'Poppins Medium',
+                color: Colors.black,
+                letterSpacing: 1),
+          ),
+          centerTitle: true,
         ),
-      ),
-    );
+        body: Container(
+            color: Color(0xFFBFFF7EB),
+            child: Column(children: <Widget>[
+              TextButton(onPressed: () {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => BottomNavBar()));
+                      }, child: Text("BACK TO NOTES")),
+              Card(
+                  elevation: 0,
+                  shape: RoundedRectangleBorder(
+                    side: BorderSide(
+                      width: 330,
+                      color: Theme.of(context).colorScheme.outline,
+                    ),
+                    borderRadius: const BorderRadius.all(Radius.circular(3)),
+                  ),
+                  child: Row(children: <Widget>[
+                    Container(
+                      child: Image.asset("assets/images/msgProfile1.jpeg",
+                          width: 50, height: 50, fit: BoxFit.cover),
+                    ),
+                    Container(
+                      child: Column(
+                        children: <Widget>[
+                          Container(
+                            margin: const EdgeInsets.fromLTRB(0, 0, 0, 0),
+                            child: Text(
+                                ' John Smith\n' +
+                                    ' 2h ago\n' +
+                                    ' It`s me again! The one and only! Nice to meet you!',
+                                textAlign: TextAlign.left,
+                                style: TextStyle(
+                                    fontFamily: 'Poppins Bold',
+                                    fontSize: 12,
+                                    color: Color.fromARGB(250, 128, 128, 128))),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ])),
+              Card(
+                  elevation: 0,
+                  shape: RoundedRectangleBorder(
+                    side: BorderSide(
+                      width: 330,
+                      color: Theme.of(context).colorScheme.outline,
+                    ),
+                    borderRadius: const BorderRadius.all(Radius.circular(3)),
+                  ),
+                  child: Row(children: <Widget>[
+                    Container(
+                      child: Image.asset("assets/images/msgProfile2.png",
+                          width: 50, height: 50, fit: BoxFit.cover),
+                    ),
+                    Container(
+                      child: Column(
+                        children: <Widget>[
+                          Container(
+                            margin: const EdgeInsets.fromLTRB(0, 0, 0, 0),
+                            child: Text(
+                                ' Rick Astely\n' +
+                                    ' 3h ago\n' +
+                                    ' Never gonna give you up! Never gonna let you down!',
+                                textAlign: TextAlign.left,
+                                style: TextStyle(
+                                    fontFamily: 'Poppins Bold',
+                                    fontSize: 12,
+                                    color: Color.fromARGB(250, 128, 128, 128))),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ])),
+              Card(
+                  elevation: 0,
+                  shape: RoundedRectangleBorder(
+                    side: BorderSide(
+                      width: 330,
+                      color: Theme.of(context).colorScheme.outline,
+                    ),
+                    borderRadius: const BorderRadius.all(Radius.circular(3)),
+                  ),
+                  child: Row(children: <Widget>[
+                    Container(
+                      child: Image.asset("assets/images/msgProfile3.jpeg",
+                          width: 50, height: 50, fit: BoxFit.cover),
+                    ),
+                    Container(
+                      child: Column(
+                        children: <Widget>[
+                          Container(
+                            margin: const EdgeInsets.fromLTRB(0, 0, 0, 0),
+                            child: Text(
+                                ' Amber Heard\n' +
+                                    ' 16h ago\n' +
+                                    ' My dog stepped on a bee. ;-(',
+                                textAlign: TextAlign.left,
+                                style: TextStyle(
+                                    fontFamily: 'Poppins Bold',
+                                    fontSize: 12,
+                                    color: Color.fromARGB(250, 128, 128, 128))),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ])),
+            ])));
   }
 }
